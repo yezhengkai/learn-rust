c3d348b1d9382ad86e86bebca8fbbb6704aa927b
kai <supon3060@gmail.com>
2024-09-14 17:06:25 +0800

    Update ch2

--- the-rust-programming-language/ch2-guessing-game/src/main.rs
+++ the-rust-programming-language/ch2-guessing-game/src/main.rs
@@ -1,15 +1,42 @@
+use rand::Rng;
+// The Rng trait defines methods that random number generators implement, and this trait must be in scope for us to use those methods.
+use std::cmp::Ordering; // Ordering type is enum and has the variants Less, Greater, and Equal
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    // println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim().eq("quit") {
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
